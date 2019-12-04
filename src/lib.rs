ec85ba43f8e07fab5d49a61f2657ea9b5ae4f12b
Terkwood <38859656+Terkwood@users.noreply.github.com>
2019-12-04 16:31:00 -0500

    Bind to GDNative (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,32 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[macro_use]
+extern crate gdnative;
+
+#[derive(gdnative::NativeClass)]
+#[inherit(gdnative::Node)]
+#[user_data(gdnative::user_data::ArcData<HelloWorld>)]
+struct HelloWorld;
+
+#[gdnative::methods]
+impl HelloWorld {
+    fn _init(_owner: gdnative::Node) -> Self {
+        HelloWorld
+    }
+
+    #[export]
+    fn _ready(&self, _owner: gdnative::Node) {
+        godot_print!("HELLO HELLO HELLO HELLO");
+        godot_print!("HELLO HELLO HELLO HELLO");
+        godot_print!("HELLO HELLO HELLO HELLO");
+        godot_print!("HELLO HELLO HELLO HELLO");
+        godot_print!("HELLO HELLO HELLO HELLO");
+        godot_print!("HELLO HELLO HELLO HELLO");
     }
 }
+
+fn init(handle: gdnative::init::InitHandle) {
+    handle.add_class::<HelloWorld>();
+}
+
+godot_gdnative_init!();
+godot_nativescript_init!(init);
+godot_gdnative_terminate!();
