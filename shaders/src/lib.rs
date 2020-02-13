f1aee68614008e2b82bde3af11316d4860af7030
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-02-13 14:01:25 -0500

    Add test

--- shaders/src/lib.rs
+++ shaders/src/lib.rs
@@ -4,9 +4,17 @@ mod boilerplate;
 
 pub struct Shader(pub String);
 #[derive(Debug)]
-pub struct Err();
+pub enum Err {
+    PngLoad,
+}
+impl From<image::ImageError> for Err {
+    fn from(_: image::ImageError) -> Err {
+        Err::PngLoad
+    }
+}
 impl Shader {
-    pub fn from(_png_rgba_bytes: &[u8]) -> Result<Shader, Err> {
+    pub fn from(png_rgba_bytes: &[u8]) -> Result<Shader, Err> {
+        let png_img = image::load_from_memory(png_rgba_bytes)?;
         Ok(Shader(boilerplate::BOGUS.to_string()))
     }
 }
@@ -23,10 +31,16 @@ mod tests {
         file.read_to_end(&mut png_in).expect("cannot read");
         png_in
     }
+
     #[test]
-    fn it_works() {
+    fn bogus_image_bytes_blow_up() {
+        let shader = Shader::from(&[0, 1, 100, 101]);
+        assert!(if let Ok(_) = shader { false } else { true })
+    }
+    #[test]
+    fn never_generate_antiquated_mushroom_stuff() {
         let shader = Shader::from(&png_bytes()).expect("fail");
         let old_boilerplate = "m = Q(m,y,0,_,_,_,_,_,B,B,B)";
-        assert_eq!(shader.0.contains(old_boilerplate), false);
+        assert!(!shader.0.contains(old_boilerplate));
     }
 }
