1e16ea15b21e47b177bba0714a5efeabba9498b5
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-30 09:21:02 -0500

    format

--- lindenmayer/src/png.rs
+++ lindenmayer/src/png.rs
@@ -1,13 +1,19 @@
-use usvg::FitTo;
 use tiny_skia::Pixmap;
+use usvg::FitTo;
 
 #[derive(Debug)]
-pub struct Size{pub width: u32, pub height: u32}
+pub struct Size {
+    pub width: u32,
+    pub height: u32,
+}
 
 pub fn convert_svg_to_png_bytes(data: &[u8]) -> (Vec<u8>, Size) {
     let out = convert_svg_to_png(data);
-    
-    let size = Size { width: out.width(), height: out.height()};
+
+    let size = Size {
+        width: out.width(),
+        height: out.height(),
+    };
 
     (out.data().to_vec(), size)
 }
@@ -16,7 +22,7 @@ fn convert_svg_to_png(data: &[u8]) -> Pixmap {
     let rtree = usvg::Tree::from_data(data, &usvg::Options::default()).unwrap();
 
     let pixmap_size = rtree.svg_node().size.to_screen_size();
-    let mut pixmap =  Pixmap::new(pixmap_size.width() as u32, pixmap_size.height() as u32).unwrap();
+    let mut pixmap = Pixmap::new(pixmap_size.width() as u32, pixmap_size.height() as u32).unwrap();
     resvg::render(&rtree, FitTo::Original, pixmap.as_mut()).unwrap();
 
     pixmap
