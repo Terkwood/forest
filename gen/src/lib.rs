fa202bb34df0aea41c6cf6d6bfb830e5e1d5d1ac
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-31 15:20:34 -0500

    stub out parse

--- gen/src/lib.rs
+++ gen/src/lib.rs
@@ -20,6 +20,8 @@ const FOREST: &str = "FF-[-F+F+F]";
 struct DrawTree {
     #[property(path = "base/start")]
     start: String,
+    #[property(path = "base/rules")]
+    rules: String,
     #[property(path = "base/iter")]
     iter: u64,
 }
@@ -28,14 +30,15 @@ fn register_properties(_builder: &ClassBuilder<DrawTree>) {}
 
 use lindenmayer::svg::DrawProps;
 
-const RULE_X_FRIENDLY: &str = "F[+X]F[-X]+X";
-const RULE_F_FRIENDLY: &str = "FF";
+const _RULE_X_FRIENDLY: &str = "F[+X]F[-X]+X";
+const _RULE_F_FRIENDLY: &str = "FF";
 
 #[gdnative::methods]
 impl DrawTree {
     fn new(_owner: &Node) -> Self {
         DrawTree {
             start: "".to_string(),
+            rules: "".to_string(),
             iter: 0,
         }
     }
@@ -48,46 +51,57 @@ impl DrawTree {
         }
 
         if let Some(start) = self.start.chars().nth(0) {
-            use lindenmayer::rule;
-
-            let (svg_bytes, svg_time) = timed(|| {
-                svg::draw_svg_utf8(DrawProps {
-                    start,
-                    iter: self.iter as usize,
-                    rules: vec![rule('X', RULE_X_FRIENDLY), rule('F', RULE_F_FRIENDLY)],
-                })
-            });
-            godot_print!(".. SVG generation in {:#?} ..", svg_time);
-
-            let ((png_bytes, size), png_time) = timed(|| png::convert_svg_to_png_bytes(&svg_bytes));
-            godot_print!("!! PNG conversion in {:#?} !!", png_time);
-            let (_, godot_time) = timed(|| {
-                let mut godot_bytes = ByteArray::new();
-                for b in png_bytes {
-                    godot_bytes.push(b)
-                }
-                let image = Image::new();
-
-                image.create_from_data(
-                    size.width as i64,
-                    size.height as i64,
-                    false,
-                    Image::FORMAT_RGBA8,
-                    godot_bytes,
-                );
-                let image_texture = unsafe { ImageTexture::new().assume_unique() };
-                image_texture.create_from_image(image, 0);
-                godot_print!("# ... IMAGE TEXTURE");
-                let sprite = Sprite::new();
-                sprite.set_texture(image_texture.upcast::<Texture>());
-                godot_print!("# ... SET TEXTURE");
-                owner.add_child(sprite.upcast::<Node>(), true)
-            });
-            godot_print!("## Godot image, texture, and sprite in {:#?}", godot_time)
+            if let Ok(rules) = parse_rules(&self.rules) {
+                self.draw(start, rules, self.iter as usize, owner)
+            }
         } else {
             godot_error!("NO START CHAR PROVIDED")
         }
     }
+
+    fn draw(&self, start: char, rules: Vec<Rule>, iter: usize, owner: &Node) {
+        let (svg_bytes, svg_time) = timed(|| {
+            svg::draw_svg_utf8(DrawProps {
+                start,
+                iter,
+                rules,
+                //rules: vec![rule('X', RULE_X_FRIENDLY), rule('F', RULE_F_FRIENDLY)],
+            })
+        });
+        godot_print!(".. SVG generation in {:#?} ..", svg_time);
+
+        let ((png_bytes, size), png_time) = timed(|| png::convert_svg_to_png_bytes(&svg_bytes));
+        godot_print!("!! PNG conversion in {:#?} !!", png_time);
+        let (_, godot_time) = timed(|| {
+            let mut godot_bytes = ByteArray::new();
+            for b in png_bytes {
+                godot_bytes.push(b)
+            }
+            let image = Image::new();
+
+            image.create_from_data(
+                size.width as i64,
+                size.height as i64,
+                false,
+                Image::FORMAT_RGBA8,
+                godot_bytes,
+            );
+            let image_texture = unsafe { ImageTexture::new().assume_unique() };
+            image_texture.create_from_image(image, 0);
+            godot_print!("# ... IMAGE TEXTURE");
+            let sprite = Sprite::new();
+            sprite.set_texture(image_texture.upcast::<Texture>());
+            godot_print!("# ... SET TEXTURE");
+            owner.add_child(sprite.upcast::<Node>(), true)
+        });
+        godot_print!("## Godot image, texture, and sprite in {:#?}", godot_time)
+    }
+}
+
+use lindenmayer::Rule;
+struct ParseErr;
+fn parse_rules(_compact: &str) -> Result<Vec<Rule>, ParseErr> {
+    todo!()
 }
 
 fn init(handle: InitHandle) {
