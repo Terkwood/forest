14b4c399d117b9f69062c7a26135bb749cdc43b7
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-31 19:01:35 -0500

    trim

--- lindenmayer/src/svg.rs
+++ lindenmayer/src/svg.rs
@@ -5,20 +5,6 @@ const INIT_DIRECTION: f32 = 0.0;
 const DEFAULT_ANGLE: f32 = 20.0;
 const DEFAULT_DISTANCE: f32 = 4.0;
 
-/*pub fn draw_svg_utf8(input: Vec<PSym<char, f32>>) -> Vec<u8> {
-    let mut v = vec![];
-
-    draw(
-        &input,
-        INIT_DIRECTION,
-        DEFAULT_ANGLE,
-        DEFAULT_DISTANCE,
-        &mut v,
-    );
-
-    v
-}*/
-
 pub struct DrawProps {
     pub start: char,
     pub rules: Vec<Rule>,
@@ -40,18 +26,3 @@ pub fn draw_svg_utf8(draw_props: DrawProps) -> Vec<u8> {
 
     v
 }
-
-/*
-#[allow(unused)]
-pub fn canned_draw_to_file() {
-    let (after, iters) = crate::develop_canned_system();
-
-    let mut file = File::create(&format!("plant_{:02}.svg", iters).to_string()).unwrap();
-    draw(
-        &after,
-        INIT_DIRECTION,
-        DEFAULT_ANGLE,
-        DEFAULT_DISTANCE,
-        &mut file,
-    );
-}*/
