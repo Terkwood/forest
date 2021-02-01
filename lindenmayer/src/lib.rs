3c5de08f55b78a76d057837836506459890ed092
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-31 19:01:00 -0500

    trim

--- lindenmayer/src/lib.rs
+++ lindenmayer/src/lib.rs
@@ -19,17 +19,11 @@ const RULE_X_1: &str = "F[+X][-X]FX";
 #[allow(unused)]
 const RULE_F_CLASSIC: &str = "FF-[-F+F+F]+[+F-F-F]";
 
-//const ITER: usize = 7;
-
 pub fn develop_system(start: char, rules: Vec<Rule>, iter: usize) -> (Vec<PSym<char, f32>>, usize) {
-    //    let axiom = symstr("X");
-
     let mut system = System::new();
     for r in rules {
         system.add_rule(r)
     }
-    //    system.add_rule(rule('X', RULE_X_FRIENDLY));
-    //    system.add_rule(rule('F', RULE_F_FRIENDLY));
     system.develop(symstr(&start.to_string()), iter)
 }
 
