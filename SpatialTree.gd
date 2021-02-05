dcf97cba27e04728601b5f1c13a6b67b120dcff4
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-02-05 07:01:31 -0500

    look for the center

--- SpatialTree.gd
+++ SpatialTree.gd
@@ -5,6 +5,9 @@ func _ready():
 	if img == null:
 		printerr("image wasn't created")
 		return
+	
+	var center_along_bottom = _guess_center_along_bottom(img)
+	print("center? %d" % center_along_bottom)
 
 	var resize_y_ratio = 1.0 * img.get_height() / img.get_width()
 	
@@ -39,3 +42,27 @@ func _make_mesh_instance(spatial_mat: SpatialMaterial, resize_y_ratio: float, ro
 	if flip_faces:
 		pm.flip_faces = true
 	return mi
+
+
+const DEFAULT_CENTER = 0.5
+# this is a poor approximation of what the "center"
+# of a tree image is assumed to be:  the first pixel
+# which is NOT transparent, along the final row in the
+# image.  the answer is returned as a percentage value
+func _guess_center_along_bottom(img: Image) -> float:
+	if img.is_empty():
+		return DEFAULT_CENTER
+	var w = img.get_width()
+	var last_row = img.get_height() - 1
+	var found_col = 0
+	
+	img.lock()
+	for x in range(0,w):
+		var c = img.get_pixel(x,last_row)
+		print("pixel %s" % c)
+		if c.a != 0.0:
+			found_col = x
+			break
+	img.unlock()
+	
+	return found_col / w
