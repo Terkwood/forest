2414448302990577cff2e8826a8fa189b562d1ba
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-02-06 16:52:38 -0500

    add controls

--- TheWoods.gd
+++ TheWoods.gd
@@ -63,3 +63,27 @@ func _ready():
 			tree.translate(Vector3(i * SPACING, 0, j * SPACING))
 			tree.rotate_y(deg2rad(randf() * 360))
 			add_child(tree)
+
+const MOVE = 0.1
+var movement = Vector2(0,0)
+func _input(event):
+	if Input.is_action_just_pressed("ui_left"):
+		movement = Vector2(-1.0 * MOVE, movement.y)
+	if Input.is_action_just_pressed("ui_right"):
+		movement = Vector2(MOVE, movement.y)
+	if Input.is_action_just_pressed("ui_up"):
+		movement = Vector2(movement.x, -1.0 * MOVE)
+	if Input.is_action_just_pressed("ui_down"):
+		movement = Vector2(movement.x, MOVE)
+
+	if Input.is_action_just_released("ui_left") and !Input.is_action_pressed("ui_right"):
+		movement = Vector2(0, movement.y)
+	if Input.is_action_just_released("ui_right") and !Input.is_action_pressed("ui_left"):
+		movement = Vector2(0, movement.y)
+	if Input.is_action_just_released("ui_up") and !Input.is_action_just_released("ui_down"):
+		movement = Vector2(movement.x, 0)
+	if Input.is_action_just_released("ui_down") and !Input.is_action_just_released("ui_up"):
+		movement = Vector2(movement.x, 0)
+
+func _physics_process(_delta):
+	$Camera.translate(Vector3(movement.x, 0, movement.y))
