29ab2ae34a2e7f6f8b149b1560ba8909d8110957
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-02-06 16:19:32 -0500

    make trees

--- TheWoods.gd
+++ TheWoods.gd
@@ -6,9 +6,55 @@ const ROWS = 10
 const COLS = 10
 const SPACING = 4
 
+class TreeParams:
+	var stroke_width = 2.0
+	var stroke_length = 4.0
+	var axiom = "F"
+	var n = 4
+	var delta = 22.5
+	var rules = "F:FF-[-F+F+F]+[+F-F-F]"
+
+var _samples = []
+
+func _make_samples():
+	var a = TreeParams.new()
+	a.rules = "F:F[+F]F[-F]F"
+	a.axiom = "F"
+	a.delta = 25.7
+	a.n = 5
+	_samples.push_front(a)
+	
+	var b = TreeParams.new()
+	b.rules = "F:F[+F]F[-F][F]"
+	b.axiom = "F"
+	b.n = 5
+	b.delta = 20.0
+	_samples.push_front(b)
+	
+	var c = TreeParams.new()
+	#_samples.push_front(c)
+	
+	var d = TreeParams.new()
+	#_samples.push_front(d)
+	
+	var e = TreeParams.new()
+	#_samples.push_front(e)
+	
+	var f = TreeParams.new()
+	#_samples.push_front(f)
+
 func _ready():
+	_make_samples()
+	
 	for i in range(0,ROWS):
 		for j in range(0, COLS):
 			var tree = SpatialTree.instance()
+			var sample = _samples[randi()%_samples.size()]
+			tree.n = sample.n
+			tree.rules = sample.rules
+			tree.axiom = sample.axiom
+			tree.delta = sample.delta
+			tree.stroke_length = sample.stroke_length
+			tree.stroke_width = sample.stroke_width
 			tree.translate(Vector3(i * SPACING, 0, j * SPACING))
 			add_child(tree)
