b86fda967df10777f0e30f77a121ed9ba556a1a9
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-02-09 16:14:51 -0500

    stubbit

--- stamp.gd
+++ stamp.gd
@@ -3,7 +3,15 @@ extends Object
 export var brush_size: float = 100.0
 export var density: float = 1.0
 
-func stamp(_owner: Node):
+func stamp(pos: Vector3, _owner: Node):
+	var transforms = _compute_transforms(pos)
+	for t in transforms:
+		printerr("write me")
 	return ERR_HELP
 	
 	
+# compute a bunch of transforms given a position, brush size,
+# and density
+func _compute_transforms(pos: Vector3) -> Array:
+	printerr("write me")
+	return []
