b53dbc94a206a53518d60b66c712daefb28bfca0
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-02-10 06:54:55 -0500

    trim aspirations

--- StampedScene.gd
+++ StampedScene.gd
@@ -6,13 +6,6 @@ onready var _samples = preload("res://SampleParams.gd").new().make_all()
 const Stamp = preload("res://stamp.gd")
 
 func _ready():
-	# let's stamp something in a few places
-	# stamp some pines,
-	# stamp some oaks
-	
-	# stamp at different densities and brush sizes
-	# do all of this manually, don't try to figure
-	# out the stamp placement on the first pass
 	var stamp = Stamp.new()
 	stamp.apply(Vector3(10.0,0.0,10.0), _samples[0], self)
 	stamp.apply(Vector3(-10.0,0.0,-10.0), _samples[1], self)
